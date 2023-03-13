56f48e6d2ddc9aee390c8fe544e9e1f09c09ea4a
hoangvu563 <119787028+hoangvu563@users.noreply.github.com>
2023-03-13 22:49:06 +0700

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> InputSet = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (InputSet.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { InputSet[target - nums[i]], i };
+                }
+                else
+                {
+                    InputSet[nums[i]] = i;
+                }
+            }
+            return null;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 1, 2, 3, 4, 5, 6, 0 };
+            int target = 5;
+            Solution test1 = new Solution();
+            test1.TwoSum(input, target);
+        }
+    }
+}
